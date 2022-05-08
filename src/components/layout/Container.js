ad78e981685038ceb84e27e1c0fb81d4fbc9431a
severino-carvalho <severinocarvalho14@gmail.com>
2022-05-08 15:54:04 -0300

    Ajustes no Navbar

---
+++ src/components/layout/Container.js
@@ -0,0 +1,9 @@
+import styles from './Container.module.css';
+
+export default function Container(props) {
+    return (
+        <div className={styles.container}>
+            {props.children}
+        </div>
+    );
+}
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,28 +2,30 @@ import { Link } from 'react-router-dom';
 import ImageVerified from '../utils/ImageVerified';
 import ImageSeverino from '../../img/Severino.jpg';
 import styles from './Navbar.module.css';
+import Container from './Container';
 
 export default function Navbar({ props }) {
     return (
         <nav className={styles.navbar}>
-
-            <div className={styles.logo}>
-                <div>
-                    <ImageVerified src={ImageSeverino} />
+            <Container>
+                <div className={styles.logo}>
+                    <div>
+                        <ImageVerified src={ImageSeverino} />
+                    </div>
+                    <div>
+                        <h2>Severino Carvalho</h2>
+                    </div>
                 </div>
-                <div>
-                    <h2>Severino Carvalho</h2>
-                </div>
-            </div>
 
-            <ul>
-                <li>
-                    <Link to='/'> Home </Link>
-                </li>
-                <li>
-                    <Link to='/contacts'> Contacts </Link>
-                </li>
-            </ul>
+                <ul>
+                    <li>
+                        <Link to='/'> Home </Link>
+                    </li>
+                    <li>
+                        <Link to='/contacts'> Contacts </Link>
+                    </li>
+                </ul>
+            </Container>
         </nav>
     );
 }
