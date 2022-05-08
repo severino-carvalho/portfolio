933a42ecb11ab7473409b838a92a765ff30fee4e
severino-carvalho <severinocarvalho14@gmail.com>
2022-05-07 22:27:11 -0300

    Start in App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 export default function App() {
   return (
-    <>
-      <h1>Portfolio</h1>
-    </>
+    <Router>
+      {/**  Navbar  */}
+
+      {/**  Routes Config */}
+      <Routes>
+        <Route path='/' element={<div>Path '/'</div>} />
+      </Routes>
+    </Router>
   );
 }
