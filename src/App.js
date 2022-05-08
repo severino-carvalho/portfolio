aa2a00282e3c5ff59b1864cc0a8f31cc6cb3a566
severino-carvalho <severinocarvalho14@gmail.com>
2022-05-08 14:33:02 -0300

    Update to the App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/layout/Navbar';
 
 export default function App() {
   return (
     <Router>
       {/**  Navbar  */}
+      <Navbar />
 
       {/**  Routes Config */}
       <Routes>
-        <Route path='/' element={<div>Path '/'</div>} />
+        <Route path='/' element={
+          <div>Path '/'</div>
+        } />
+
+        <Route path='/projects' element={
+          <div>
+            Path '/projects'
+          </div>
+        } />
       </Routes>
     </Router>
   );
