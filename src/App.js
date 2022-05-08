75801d49acfdc77aff460a85d33bc8f7b665db4b
severino-carvalho <severinocarvalho14@gmail.com>
2022-05-08 15:55:53 -0300

    Add container component in the App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Container from './components/layout/Container';
+import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
 
 export default function App() {
   return (
     <Router>
+
       {/**  Navbar  */}
       <Navbar />
 
-      {/**  Routes Config */}
-      <Routes>
-        <Route path='/' element={
-          <div>Path '/'</div>
-        } />
+      <Container>
 
-        <Route path='/projects' element={
-          <div>
-            Path '/projects'
-          </div>
-        } />
-      </Routes>
+        {/**  Routes Config */}
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/contacts' element={<div>Path '/projects'</div>} />
+        </Routes>
+
+      </Container>
+
+      {/**  Footer  */}
+      <Footer />
     </Router>
   );
 }
