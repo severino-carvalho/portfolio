5a0eef4cb7586e607b444409cddbe037e82868f0
severino-carvalho <severinocarvalho14@gmail.com>
2022-05-08 21:59:12 -0300

    Protótipo do footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,7 +1,35 @@
+import { Link } from 'react-router-dom';
+import { MdEmail, MdPhone } from 'react-icons/md';
+import Container from './Container';
+import styles from './Footer.module.css';
+
 export default function Footer() {
     return (
-        <footer>
-            
+        <footer className={styles.footer}>
+            <Container >
+                <div className={styles.content}>
+                    <div>
+                        <h2>Contact</h2>
+                        <ul>
+                            <li>
+                                <Link to=''>
+                                    <MdEmail /> severinocarvalho14@gmail.com
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to=''>
+                                    <MdPhone /> +55 (84) 99465-0540
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to=''>
+
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </Container>
         </footer>
     );
 }
