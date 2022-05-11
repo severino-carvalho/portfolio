b752020ad821867793d8766e9d8fcc0481605856
severino-carvalho <severinocarvalho14@gmail.com>
2022-05-11 12:46:24 -0300

    Adding new styles and text

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -11,31 +11,33 @@ export default function Home() {
                         <div>
                             <p>
                                 {`Hello, I'm Severino Carvalho, I have 20 years old. I'm Computer Technician by `}
-                                < b >
-                                    <a href="https://portal.ifrn.edu.br/campus/joaocamara" target={'_blank'} rel={'noreferrer'}>
-                                        {`IFRN-JC`}
-                                    </a>
-                                </b>
+                                <a href="https://portal.ifrn.edu.br/campus/joaocamara" target={'_blank'} rel={'noreferrer'}>
+                                    {`IFRN-JC`}
+                                </a>
                                 {` (Instituto Federal de Educação Ciência e Tecnologia do Rio Grande do Norte - IFRN `}
                                 <i>Campus</i>
                                 {` João Câmara) and Internet Programming by `}
-                                <b>
-                                    <a href="https://www.metropoledigital.ufrn.br/portal/ensino/tecnico" target={'_blank'} rel={'noreferrer'}>
-                                        IMD
-                                    </a>
-                                </b>
+                                <a href="https://www.metropoledigital.ufrn.br/portal/ensino/tecnico" target={'_blank'} rel={'noreferrer'}>
+                                    IMD
+                                </a>
                                 {` (Instituto Metrópole Digital - IMD/UFRN).`}
+                            </p>
+
+                            <p>
+                                {`I am currently waiting for the second semester to start my degree in `}
+                                <a href="https://www.ect.ufrn.br/" target={'_blank'} rel={'noreferrer'}>
+                                    {`CeT`}
+                                </a>
+                                {` (Escola de Ciencias e Tecnologia) at `}
+                                <a href="https://www.ufrn.br/" target={'_blank'} rel={'noreferrer'}>
+                                    {`UFRN`}
+                                </a>
+                                {` (Universidade Federal do Rio Grande do Norte).`}
                             </p>
                         </div>
                         <div className={styles.sectionImage}>
                         </div>
                     </div>
-                </section>
-
-                <section>
-                    <p>
-                        {`Hello World...`}
-                    </p>
                 </section>
             </article>
         </main >
