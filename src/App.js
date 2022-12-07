04528302c76621ee0455d1ab6cfd564596b254d2
Severino <severinocarvalho14@gmail.com>
2022-12-06 23:44:15 -0300

    Update: Identação

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Container from './components/layout/Container';
-import Footer from './components/layout/Footer';
-import Navbar from './components/layout/Navbar';
-import Home from './components/pages/Home';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Container from "./components/layout/Container";
+import Footer from "./components/layout/Footer";
+import Navbar from "./components/layout/Navbar";
+import Home from "./components/pages/Home";
 
 export default function App() {
   return (
     <Router>
-
       {/**  Navbar  */}
       <Navbar />
 
       <Container>
-
         {/**  Routes Config */}
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/contacts' element={<div>Path '/projects'</div>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/contacts" element={<div>Path '/projects'</div>} />
         </Routes>
-
       </Container>
 
       {/**  Footer  */}
