acb230aceb3bc1205bac3784217480d4d3389806
Severino <severinocarvalho14@gmail.com>
2022-12-06 23:43:26 -0300

    Update: Atualização na cor de fundo da página (sobre mim)

--- src/components/layout/Container.js
+++ src/components/layout/Container.js
@@ -1,9 +1,5 @@
-import styles from './Container.module.css';
+import styles from "./Container.module.css";
 
 export default function Container(props) {
-    return (
-        <div className={styles.container}>
-            {props.children}
-        </div>
-    );
+  return <div className={styles.container}>{props.children}</div>;
 }
