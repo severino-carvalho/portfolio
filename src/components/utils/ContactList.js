f1c767d6bfd595a0252682c60d07b8bb086082ce
Severino <severinocarvalho14@gmail.com>
2022-12-11 13:05:08 -0300

    Update: Otimização de código e tamanho

---
+++ src/components/utils/ContactList.js
@@ -0,0 +1,27 @@
+import styles from "./ContactList.module.css";
+
+export default function InfoList({ title, list }) {
+  return (
+    <div className={styles.infoList}>
+      <h3>{title}</h3>
+      <ul className={styles.list_contact}>
+        {list.map((item, index) => {
+          return (
+            <li key={index}>
+              <a
+                className={styles.link_contact}
+                href={item.href}
+                target={item.target}
+                rel={"noreferrer"}
+                title={item.title}
+              >
+                <span className="icon_contact">{item.icon}</span>{" "}
+                <span>{item.text}</span>
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
