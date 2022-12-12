4e064fbaf694122e2e468e9ea8a71c0c68280243
Severino <severinocarvalho14@gmail.com>
2022-12-12 19:48:56 -0300

    Create: Criação de arquivos para pagina "Projetos"

---
+++ src/components/pages/Project.js
@@ -0,0 +1,12 @@
+import Container from "../layout/Container";
+import styles from "./Project.module.css";
+
+export default function Project() {
+  return (
+    <Container>
+      <main>
+        
+      </main>
+    </Container>
+  );
+}
